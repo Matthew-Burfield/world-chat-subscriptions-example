725c42e54937e37f9ffc3e7b2cf98f476c3ee972
Nikolas Burk <nikolas.burk@gmail.com>
2017-02-28 20:32:49 +0100

    added tutorial link

--- src/Banner.js
+++ src/Banner.js
@@ -8,11 +8,17 @@ export default class Banner extends Component {
       <div className='Banner'>
         <img src={require('./assets/info.svg')} alt="info"/>
         <div className='InfoText'>This app is using <b>GraphQL Subscriptions</b> with <b>Apollo Client</b></div>
-        <div className='ReadTutorialButton'>Read Tutorial</div>
+        <div
+          className='ReadTutorialButton'
+          onClick={() => this._openTutorial()}
+        >Read Tutorial</div>
       </div>
     )
   }
 
-
+  _openTutorial = () => {
+    const win = window.open('https://dev.graph.cool/docs/tutorials/worldchat-subscriptions-example-ui0eizishe', '_blank');
+    win.focus();
+  }
 
 }
